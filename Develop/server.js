e9a963d4114a6216762d78325cc478118c652640
Carina Ullman <carinalullman@gmail.com>
2020-02-02 21:57:48 -0700

    Added Some Routes

---
+++ Develop/server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3003;
+
+const db = require(".models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+// need routes here
+db.workout.create({ name: "Workout Tracker" })
+    .then(dbworkout => {
+        console.log(dbworkout);
+    })
+    .catch(({ message }) => {
+        console.log(message);
+    });
+
+app.post("/submit", ({ body }, res) => {
+    db.workout.create(body)
+        .then(({ _id }) => db.workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
+        .then(dbworkout => {
+            res.json(dbworkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/exercise", (req, res) => {
+    db.exercise.find({})
+        .then(dbexercise => {
+            res.json(dbexercise);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/stats", (req, res) => {
+    db.workout.find({})
+        .then(dbworkout => {
+            res.json(dbworkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+//update this when you're not tired :)
+// app.get("/populated", (req, res) => {
+//     db.Library.find({})
+//         .populate("books")
+//         .then(dbLibrary => {
+//             res.json(dbLibrary);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});

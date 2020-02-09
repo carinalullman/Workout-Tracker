71b2c91c69577bf88de042ef085172a895ac1b4b
Carina Ullman <carinalullman@gmail.com>
2020-02-09 12:33:49 -0700

    Submit Buttons are being tricky

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = mongojs(databaseUrl, collections);
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
